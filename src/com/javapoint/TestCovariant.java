a98c2a3e60fd2e93c6ac4157a180df51dcf5653d
Hamza Khan <hamza0559119811@gmail.com>
2020-06-25 02:16:42 +0500

    TestCovariant

---
+++ src/com/javapoint/TestCovariant.java
@@ -0,0 +1,16 @@
+package com.javapoint;
+class A{
+    A get(){return this;}
+}
+class B1 extends A{
+    B1 get(){
+        return this;
+    }
+
+    void message(){
+        System.out.println("Welcome to Covariant type");
+    }
+    public static void main(String[] args) {
+    new B1().get().message();
+    }
+}
