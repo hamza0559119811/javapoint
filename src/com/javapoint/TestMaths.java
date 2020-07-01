0a4fc57ce42b8d4218b1a5d14021d2a6442db428
Hamza Khan <hamza0559119811@gmail.com>
2020-07-02 03:12:52 +0500

    TestMaths

---
+++ src/com/javapoint/TestMaths.java
@@ -0,0 +1,16 @@
+package com.javapoint;
+
+public class TestMaths {
+    public static void main(String[] args) {
+        double x = 29;
+        double y = 5;
+        System.out.println("Maximum number "+ Math.max(x,y));
+        System.out.println("Squareroot of y "+ Math.sqrt(y));
+        System.out.println("x raised to y "+ Math.pow(x, y));
+        System.out.println("Log(x) " + Math.log(x));
+        System.out.println("log(y) "+ Math.log(y));
+        System.out.println("log base 10 of y   "+ Math.log10(y));
+        System.out.println("end");
+
+    }
+}
